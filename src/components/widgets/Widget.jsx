96528b56b850ad8b26d2cbc4ed7cf71cf42ccdf6
Tolga İzdaş <tolgaizdas@gmail.com>
2022-12-12 16:11:59 +0300

    added an icon to drag the widget

--- src/components/widgets/Widget.jsx
+++ src/components/widgets/Widget.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Draggable from "react-draggable"; // The default
+import DragHandleIcon from "@mui/icons-material/DragHandle";
 
 export default function Widget(props) {
   return (
-    <Draggable>
+    <Draggable handle="strong">
       <div className="widget">
         <div className="widget-header">
+          <strong style={{ alignItems: "right" }}>
+            <DragHandleIcon fontSize="large" />
+          </strong>
           <h2>{props.widgetName}</h2>
           <p>{props.widgetDescription}</p>
         </div>
